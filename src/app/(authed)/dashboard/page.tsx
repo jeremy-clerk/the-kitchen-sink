d875477273e4d86178233482ccb6d19ecce35b79
Jeremy Wright <jeremy@clerk.dev>
2024-11-26 08:25:46 -0700

    Testing paths

--- src/app/(authed)/dashboard/page.tsx
+++ src/app/(authed)/dashboard/page.tsx
@@ -4,7 +4,7 @@ export default function Dashboard() {
   return (
     <div className={"h-full flex p-6 w-full"}>
       <div className={"bg-slate-600 p-2 rounded-md w-full max-w-lg"}>
-        <SignOutButton signOutOptions={{ redirectUrl: "/sign-in" }}>
+        <SignOutButton signOutOptions={{ redirectUrl: "/sign-up" }}>
           <button
             className={
               "rounded-md p-2 bg-gradient-to-r capitalize shadow from-purple-900 to-purple-500"
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,10 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 
 export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) await auth.protect();
+  if (!isPublicRoute(request))
+    await auth.protect({
+      unauthenticatedUrl: new URL("/sign-in", request.url).toString(),
+    });
 });
 
 export const config = {
